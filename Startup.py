541ed360c403230db41b68153d377e663128583b
madzing <janmetzing@gmx.de>
2015-11-09 08:48:15 +0100

    Improved Startup

--- Startup.py
+++ Startup.py
@@ -10,25 +10,20 @@ time.sleep(4)
 ###
 
 gui = GuiService()
-serialRead = ReadSerial(gui.getPort(),gui.getBaudRate(),"$GPGGA.log","$GPRMC.log","w")
-serialRead.start()
 while True:
-    time1 = time.time()
-    if gui.getIsTrigger():   
+    if gui.getIsTrigger():
+        serialRead = ReadSerial(gui.getPort(),gui.getBaudRate(),"$GPGGA.log","$GPRMC.log","w")
+        serialRead.start()
         importer = GpsImportService(gui)
         trigger = TriggerService(gui,importer)
 
-    while gui.getIsTrigger():
-        runTime = time.time()
-        ####################################################
-        importer.go()
-        trigger.go()
-        ####################################################
-        endTime = time.time()
-        wait = round((1/gui.getRunsPerSecond())-(endTime-runTime),5)
-        if wait > 0:
-            time.sleep(wait)
-    time2 = time.time()
-    sleep = round((((1/10)-(time2-time1))),5)
-    if sleep > 0:
-        time.sleep(sleep)
+        while gui.getIsTrigger():
+            runTime = time.time()
+            ####################################################
+            importer.go()
+            trigger.go()
+            ####################################################
+            endTime = time.time()
+            wait = round((1/gui.getRunsPerSecond())-(endTime-runTime),5)
+            if wait > 0:
+                time.sleep(wait)
