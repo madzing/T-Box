c823436706a3f748ab284dd210dabfd1eb0fbff1
madzing <janmetzing@gmx.de>
2015-11-11 11:05:35 +0100

    Improved GuiService

--- GuiService.py
+++ GuiService.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class GuiService():   
+class GuiService():
     def readSettingsFile(self,wert):
         settingsFile = open("Settings","r")
         file = settingsFile.read()
@@ -10,80 +10,54 @@ class GuiService():
                 x = element.split(":")
                 return x[1]
     def getPort(self):
-        try:
+
             self.port = self.readSettingsFile("Port")
-            return self.port
-        except:
             return self.port
 
     def getBaudRate(self):
-        try:
             self.baudRate = self.readSettingsFile("Baud")
             return self.baudRate
-        except:
-            return self.baudRate
-            
+
     def getIsTrigger(self):
-        try:
             if self.readSettingsFile("Trigger") == "True":
                 self.isTrigger = True
             else:
                 self.isTrigger = False
             return self.isTrigger
-        except:
-            return self.isTrigger
 
     def getDesiredDistance(self):
-        try:
             self.desieredDistance = float(self.readSettingsFile("Desiered Distance"))
-            return self.desieredDistance
-        except:
             return self.desieredDistance
 
     def getAcceptableDeviation(self):
-        try:
             self.acceptableDeviation = float(self.readSettingsFile("Acceptable Deviation"))
-            return self.acceptableDeviation
-        except:
             return self.acceptableDeviation
 
     def getRunsPerSecond(self):
-        try:
             self.runsPerSecond = float(self.readSettingsFile("Runs per Second"))
             return self.runsPerSecond
-        except:
-            return self.runsPerSecond 
 
     def getSmoothMode(self):
-        try:
             if int(self.readSettingsFile("SecList"))==1:
                 self.Smoothmode = "secList"
-        
+
             elif int(self.readSettingsFile("Gprmc"))==1:
                 self.Smoothmode = "GPRMC"
-        
+
             elif int(self.readSettingsFile("Simple"))==1:
                 self.Smoothmode = "simple"
             return self.Smoothmode
-        except:
-            return self.Smoothmode
 
     def getLogFile(self):
-        try:
             if int(self.readSettingsFile("Gprmc"))==1:
                 self.logFile = "$GPRMC.log"
-        
+
             else:
                 self.logFile = "$GPGGA.log"
             return self.logFile
-        except:
-            return self.logFile
 
     def getListDimensions(self):
-        try:
-            self.listDimensions = [int(self.readSettingsFile("Length of rawlist")),int(self.readSettingsFile("Length of smoothlist"))]
-            return self.listDimensions
-        except:
+            self.listDimensions = [float(self.readSettingsFile("Length of rawlist")),float(self.readSettingsFile("Length of smoothlist"))]
             return self.listDimensions
 
     def __init__(self):
@@ -96,4 +70,3 @@ class GuiService():
         self.Smoothmode = "secList"
         self.logFile = "$GPGGA.log"
         self.listDimensions = [20,2]
-        
